90c7c511d72306277ee4005a76947073075fb7a2
mehdi cherti <mehdi@cherti.name>
2015-06-29 14:08:39 +0200

    add libs

---
+++ datasets.py
@@ -0,0 +1,42 @@
+import numpy as np
+from sklearn.preprocessing import StandardScaler, LabelEncoder
+from sklearn.utils import shuffle
+import pandas as pd
+
+from sklearn.datasets import fetch_lfw_people
+
+def build_lfw():
+    lfw_people = fetch_lfw_people(min_faces_per_person=70, resize=0.2)
+    X = lfw_people.data
+    X = X.astype(np.float32) / 255.
+    return X, lfw_people.images.shape[1:]
+
+from sklearn.datasets import load_digits
+def build_digits():
+    digits = load_digits()
+    X = digits.data
+    X = X.astype(np.float32) / 16.
+    return X, digits.images.shape[1:]
+
+from lasagne.datasets.mnist import MNIST
+def build_mnist():
+    data = MNIST()
+    data.load('all')
+    X = data.X
+    X = X.astype(np.float32)
+    return X, (28, 28)
+
+from lasagne.datasets.cifar10 import Cifar10
+def build_cifar10():
+    data = Cifar10()
+    data.load('all')
+    X = data.X
+    X = X.astype(np.float32)
+    return X, (3, 32, 32)
+
+datasets = dict(
+    lfw=build_lfw,
+    digits=build_digits,
+    mnist=build_mnist,
+    cifar10=build_cifar10
+)
